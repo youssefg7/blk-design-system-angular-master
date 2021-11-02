038a7601e84be723d01c04dd9fe175565839fae7
youssefg7 <ygfssms@gmail.com>
2021-11-02 14:22:47 +0200

    Register, login forms validation

--- src/app/pages/examples/registerpage/registerpage.component.ts
+++ src/app/pages/examples/registerpage/registerpage.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy, HostListener } from "@angular/core";
-import { FormGroup, FormBuilder, FormArray, FormControl } from "@angular/forms";
+import { FormGroup, FormBuilder, FormArray, FormControl, Validators } from "@angular/forms";
 @Component({
   selector: "app-registerpage",
   templateUrl: "registerpage.component.html"
@@ -9,15 +9,20 @@ export class RegisterpageComponent implements OnInit, OnDestroy {
   focus;
   focus1;
   focus2;
+  focus3;
+  focus4;
+  registerAttempt;
+  loginAttempt;
+
   registerForm = new FormGroup({
-    name: new FormControl(),
-    password: new FormControl(),
-    email: new FormControl(),
-    isAdmin: new FormControl()
+    name: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl('',[Validators.required,Validators.minLength(6)]),
+    isAdmin: new FormControl(false)
   });
   loginForm = new FormGroup({
-    email: new FormControl(),
-    password: new FormControl()
+    email: new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl('',[Validators.required,Validators.minLength(6)])
   })
   constructor(private fb: FormBuilder) { }
 
@@ -25,6 +30,8 @@ export class RegisterpageComponent implements OnInit, OnDestroy {
   ngOnInit() {
     var body = document.getElementsByTagName("body")[0];
     body.classList.add("register-page");
+    this.registerAttempt=false;
+    this.loginAttempt=false;
   }
   
 
@@ -32,12 +39,44 @@ export class RegisterpageComponent implements OnInit, OnDestroy {
   onSubmitRegister(): void {
     console.log(this.registerForm.value);
     console.log('register fn');
+    this.registerAttempt = true;
+    if(this.registerForm.valid){
+      this.registerAttempt = false;
+    }
   }
   onSubmitLogin(): void {
     console.log(this.loginForm.value);
     console.log('login fn');
+    this.loginAttempt = true;
+    if(this.loginForm.valid){
+      this.loginAttempt = false;
+    }
   }
   
+  get name(){
+    return this.registerForm.get('name');
+  }
+
+  get newPassword(){
+    return this.registerForm.get('password');
+  }
+
+  get newEmail(){
+    return this.registerForm.get('email');
+  }
+
+  get isAdmin(){
+    return this.registerForm.get('isAdmin');
+  }
+
+  get loginEmail(){
+    return this.loginForm.get('email');
+  }
+
+  get loginPassword(){
+    return this.loginForm.get('password');
+  }
+
   ngOnDestroy() {
     var body = document.getElementsByTagName("body")[0];
     body.classList.remove("register-page");
