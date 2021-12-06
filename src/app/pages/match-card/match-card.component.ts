6c9c2862528a3ee05ce3d8a99b624e871cb5ec7f
Anthony-Amgad <19p9880@eng.asu.edu.eg>
2021-12-06 18:35:29 +0200

    match card independent component

--- src/app/pages/match-card/match-card.component.ts
+++ src/app/pages/match-card/match-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-match-card',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MatchCardComponent implements OnInit {
 
   constructor() { }
-
+  @Input() tsmatch;
   ngOnInit(): void {
   }
 
