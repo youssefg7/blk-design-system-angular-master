7b72952dfc8de5713e04b4b3ee564446864f0957
Anthony-Amgad <19p9880@eng.asu.edu.eg>
2021-12-26 22:18:56 +0200

    password change bug fix

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable,OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { ToastrService } from 'ngx-toastr';
 import { Observable, from, Subject } from 'rxjs';
+import { User } from '../models/user.model';
 import { UserService } from './user.service';
 
 
@@ -19,7 +21,7 @@ export class AuthService {
 
 
 
-  constructor(private toastr:ToastrService, private userService:UserService,private router: Router, private afAuth: AngularFireAuth, private cookieService:CookieService) { 
+  constructor(private toastr:ToastrService, private userService:UserService,private router: Router, private afAuth: AngularFireAuth, private cookieService:CookieService, private afs:AngularFirestore) { 
     this.userLoggedIn = false;
     this.cred = this.afAuth.onAuthStateChanged((user) => {
       if (user) {
@@ -91,12 +93,13 @@ export class AuthService {
       });
   }
 
-  authUpdatePassword(pass:string){
+  async authUpdatePassword(pass:string){
     
     this.afAuth.authState.subscribe( item => item.updatePassword(pass).then(() => {
       this.toastr.success('Success!', 'Password Changed', {
         timeOut: 3000,
       });
+      this.afs.collection<User>('users').doc(this.cookieService.get('Uid')).update({password:pass});
     }).catch((error) => {
       if(pass.length < 6){
         this.toastr.error('has to be more than 6 char','New Password invalid' , {
